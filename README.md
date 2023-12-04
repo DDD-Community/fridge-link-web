f958001096d17a7b88433914c7746932bf79f49f
hyeseon han <hyeseon405@gmail.com>
2023-12-05 00:23:52 +0900

    ci: github actions 초기 세팅

--- README.md
+++
@@ -1,2 +0,0 @@
-# mara-web
-DDD 10기 web1 팀 web front 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,9 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"></div>
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        next js
+      </div>
     </main>
   );
 }
