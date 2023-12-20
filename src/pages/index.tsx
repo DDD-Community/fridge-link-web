b1cbff57babeb0f4254c41d7751d6d64179c27e5
hyeseon han <hyeseon405@gmail.com>
2023-12-20 23:13:03 +0900

    style: 워딩 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function Home (): JSX.Element {
+export default function Home(): JSX.Element {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        next js
+        mara web
       </div>
     </main>
-  )
+  );
 }
