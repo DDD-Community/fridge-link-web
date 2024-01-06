80e5f8cf8b11aa1fc04b934a947a46cd44d17d56
a-honey <jungahyeon0512@khu.ac.kr>
2024-01-06 15:16:30 +0900

    MARA-10 chore: add Jira commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
+  extends: ['@commitlint/config-conventional'],
   rules: {
-    "scope-enum": [2, "always", []],
+    'subject-empty': [0],
+    'type-empty': [0],
+    'type-enum': [0],
+    'scope-case': [0],
+    'references-empty': [0],
+    'subject-full-stop': [0],
   },
 };
