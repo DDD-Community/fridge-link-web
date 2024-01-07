03632347f1106d904b16b31a408ce657d3faa36a
a-honey <jungahyeon0512@khu.ac.kr>
2024-01-07 12:16:39 +0900

    fix: just config-conventional MARA-10

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,3 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'subject-empty': [0],
-    'type-empty': [0],
-    'type-enum': [0],
-    'scope-case': [0],
-    'references-empty': [0],
-    'subject-full-stop': [0],
-  },
 };
