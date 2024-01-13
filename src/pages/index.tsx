11ccbd8b71ce4713e1f568f3daaf2844d36a09fe
a-honey <jungahyeon0512@khu.ac.kr>
2024-01-13 23:03:07 +0900

    fix: merge conflict resolve

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,15 @@
 import AppleIconSvg from '../assets/icons/Frame 35.svg';
 import { AppleIcon } from '@/components/atoms/icons/FoodIcons';
-import { Inter } from 'next/font/google';
+import { type NextPage } from 'next';
 
-const inter = Inter({ subsets: ['latin'] });
-
-export default function Home(): JSX.Element {
+const Home: NextPage = () => {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        mara web
-        <AppleIconSvg />
-        <AppleIcon />
-      </div>
+    <main className={`flex min-h-screen justify-center items-center p-24 `}>
+      <div className="heading1-bold text-primary2">mara</div>
+      <p className="body2-semibold text-point1">web</p>
+      <AppleIconSvg />
+      <AppleIcon />
     </main>
   );
-}
+};
+export default Home;
