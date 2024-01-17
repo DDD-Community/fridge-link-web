161af009c549a5337c06a6d8b6fda10bb8762081
a-honey <jungahyeon0512@khu.ac.kr>
2024-01-18 04:20:53 +0900

    feat: 식자재 추가 모달 오픈 추가 및 닫기 전달

--- src/components/atoms/GeenButton.tsx
+++ src/components/atoms/GeenButton.tsx
@@ -6,10 +6,15 @@ interface GreenButtonProps {
   className: string;
 }
 
-const GreenButton: React.FC<GreenButtonProps> = ({ text, className }) => {
+const GreenButton: React.FC<GreenButtonProps> = ({
+  text,
+  className,
+  handler,
+}) => {
   return (
     <button
       className={`bg-primary2 text-white p-18 gap-12 rounded-12 heading4-semibold ${className}`}
+      onClick={handler}
     >
       {text}
     </button>
--- src/components/atoms/ModalBottom.tsx
+++ src/components/atoms/ModalBottom.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 
-const ModalBox: React.FC = () => {
+interface ModalBoxProps {
+  children: React.ReactNode;
+  blackClickHandler: () => void;
+}
+
+const ModalBox: React.FC<ModalBoxProps> = ({ children, blackClickHandler }) => {
+  const handleWhiteContentClick: (e: React.MouseEvent) => void = (e) => {
+    e.stopPropagation();
+    console.log('받아온 이벤트를 실행');
+  };
+
   return (
-    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white p-4">
-        <div>hi</div>
+    <div
+      className="fixed top-0 left-0 w-full h-full flex items-end justify-center bg-black bg-opacity-50 z-[9999]"
+      onClick={blackClickHandler}
+    >
+      <div
+        className="w-full h-[500px] mb-[-24px] bg-white p-4 rounded-[24px]"
+        onClick={handleWhiteContentClick}
+      >
+        {children}
       </div>
     </div>
   );
