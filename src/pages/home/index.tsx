b690db8d39cc056508337b66154ee31182d1334e
hyeseon han <hyeseon405@gmail.com>
2024-01-18 01:15:54 +0900

    fix: page file directory 생성, home page 적용

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,36 +4,51 @@ import FriendsFridgeIconSvg from '@/assets/icons/IMG/Home/img_home_friend.svg';
 import { GreenLink } from '@/components/atoms';
 import { NearExpirationWarnBox, SvgAndTextBox } from '@/components/molecules';
 import { IngredientBoard } from '@/components/organisms';
+import Header from '@/components/organisms/Header';
+import AlarmIcon from '@/assets/icons/ICON/COMMON/ic_alert.svg';
 
 const Home: NextPage = () => {
   const isNearExpirationWarn = true;
   return (
-    <section className={`flex flex-col min-h-screen p-20 bg-gray1`}>
-      {isNearExpirationWarn && (
-        <NearExpirationWarnBox className="mt-12" count={4} />
-      )}
-      <div className="flex gap-8.5 mt-12">
-        <SvgAndTextBox
-          svgComponent={<MyFridgeIconSvg width={72} height={72} />}
-          text="내 냉장고"
-          linkTo="/myfridge"
-        />
-        <SvgAndTextBox
-          svgComponent={<FriendsFridgeIconSvg width={97} height={72} />}
-          text="친구 냉장고"
-          linkTo="/friendfridge"
-        />
-      </div>
-      <div>
-        <div className="text-gray6 mt-30 mb-12 heading4-semibold">소비기한</div>
-        <IngredientBoard />
-      </div>
-      <GreenLink
-        className="mt-14"
-        text="내 냉장고 확인하기"
-        linkTo="myfridge"
+    <div className="pt-[52px]">
+      <Header
+        headerTitle={''}
+        headerLeft={<div className="w-[100px] h-3 bg-point2" />}
+        headerRight={
+          <div>
+            <AlarmIcon />
+          </div>
+        }
       />
-    </section>
+      <section className={`flex flex-col min-h-screen p-20 bg-gray1`}>
+        {isNearExpirationWarn && (
+          <NearExpirationWarnBox className="mt-12" count={4} />
+        )}
+        <div className="flex gap-8.5 mt-12">
+          <SvgAndTextBox
+            svgComponent={<MyFridgeIconSvg width={72} height={72} />}
+            text="내 냉장고"
+            linkTo="/myfridge"
+          />
+          <SvgAndTextBox
+            svgComponent={<FriendsFridgeIconSvg width={97} height={72} />}
+            text="친구 냉장고"
+            linkTo="/friendfridge"
+          />
+        </div>
+        <div>
+          <div className="text-gray6 mt-30 mb-12 heading4-semibold">
+            소비기한
+          </div>
+          <IngredientBoard />
+        </div>
+        <GreenLink
+          className="mt-14"
+          text="내 냉장고 확인하기"
+          linkTo="myfridge"
+        />
+      </section>
+    </div>
   );
 };
 export default Home;
