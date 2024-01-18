519661f2c5b050f507ebfcdc250081c379fdb49b
a-honey <jungahyeon0512@khu.ac.kr>
2024-01-18 12:43:50 +0900

    feat: 제출 완료시 토스트메시지 추가

--- src/pages/fridge/index.tsx
+++ src/pages/fridge/index.tsx
@@ -6,18 +6,31 @@ import {
 } from '@/components/organisms';
 import { type NextPage } from 'next';
 import { useState } from 'react';
+import { ToastMessage } from '@/components/atoms';
 
 const FridgePage: NextPage = () => {
   const [isOpenIngredientAddModal, setIsOpenIngredientAddModal] =
     useState(false);
+  const [isOpenToastMessage, setIsOpenToastMessage] = useState(false);
+
+  const toggleIsOppenToastMessage: () => void = () => {
+    setIsOpenToastMessage((prev) => !prev);
+  };
 
   const toggleIsOpenIngredientAddModal: () => void = () => {
     setIsOpenIngredientAddModal((prev) => !prev);
   };
   return (
     <>
+      {isOpenToastMessage && (
+        <ToastMessage
+          text="식자재 추가가 완료되었습니다."
+          toggleHandler={toggleIsOppenToastMessage}
+        />
+      )}
       {isOpenIngredientAddModal && (
         <IngredientAddModal
+          toggleIsOppenToastMessage={toggleIsOppenToastMessage}
           toggleIsOpenIngredientAddModal={toggleIsOpenIngredientAddModal}
         />
       )}
