b5cb324acf759213491180042caf488760c8c4e6
hyeseon han <hyeseon405@gmail.com>
2024-01-28 02:07:50 +0900

    chore: lint 수정

--- src/components/atoms/Radio.tsx
+++ src/components/atoms/Radio.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const Radio: React.FC<{ checked: boolean }> = ({ checked }) => {
-    return checked ? (
-        <div className="flex justify-center items-center w-[22px] aspect-square rounded-full border-2 border-primary2">
-            <div className="w-[12px] aspect-square rounded-full bg-primary2" />
-        </div>
-    ) : (
-        <div className="w-[22px] aspect-square rounded-full border-2 border-gray2" />
-    );
+  return checked ? (
+    <div className="flex justify-center items-center w-[22px] aspect-square rounded-full border-2 border-primary2">
+      <div className="w-[12px] aspect-square rounded-full bg-primary2" />
+    </div>
+  ) : (
+    <div className="w-[22px] aspect-square rounded-full border-2 border-gray2" />
+  );
 };
 
 export default Radio;
