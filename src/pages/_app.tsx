0c2593fab261fdc99d0529804538ec4ebb4e5fdd
hyeseon han <hyeseon405@gmail.com>
2024-01-27 16:04:24 +0900

    chore: dayjs locale 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 import 'dayjs/locale/ko';
+import dayjs from 'dayjs';
+dayjs.locale('ko');
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
