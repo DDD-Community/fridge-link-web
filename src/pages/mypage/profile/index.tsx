5969c2192c63be27d92425d7cd2000fc81ff724e
a-honey <jungahyeon0512@khu.ac.kr>
2024-01-30 21:56:31 +0900

    fix: 헤더 backgroundColor

--- src/pages/mypage/profile/index.tsx
+++ src/pages/mypage/profile/index.tsx
@@ -4,6 +4,7 @@ import ProfileImg from '@/assets/profile.png';
 import { Button } from '@/components/atoms';
 import React, { useEffect, useState } from 'react';
 import { ExclamationIcon } from '@/assets/icons';
+import Header from '@/components/organisms/Header';
 
 const FriendsListPage: NextPage = () => {
   const [selectedImageSrc, setSelectedImageSrc] = useState(ProfileImg);
@@ -49,33 +50,7 @@ const FriendsListPage: NextPage = () => {
 
   return (
     <div className={'bg-white pt-[52px] min-h-screen'}>
-      <div
-        className={`flex justify-center items-center fixed top-0 w-screen max-w-[480px] py-[12px] px-[20px] z-[1000]`}
-      >
-        <div
-          onClick={() => {
-            window.history.back();
-          }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 20 20"
-            fill="none"
-          >
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M14.6446 1.41789C15.0351 1.80842 15.0351 2.44158 14.6446 2.83211L7.47671 10L14.6446 17.1679C15.0351 17.5584 15.0351 18.1916 14.6446 18.5821C14.2541 18.9726 13.6209 18.9726 13.2304 18.5821L4.64828 10L13.2304 1.41789C13.6209 1.02737 14.2541 1.02737 14.6446 1.41789Z"
-              fill="#363A45"
-            />
-          </svg>
-        </div>
-        <div className="flex justify-center text-center w-full">
-          <p className="heading3-bold text-gray8 pt-1">프로필</p>
-        </div>
-      </div>
+      <Header headerTitle={'프로필'} backgroundColor="white" />
       <section
         className={`flex flex-col items-center gap-[20px] min-h-screen p-20 bg-white`}
       >
