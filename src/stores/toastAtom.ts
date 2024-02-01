29b42858f33e1c66c38ea2348e4c6d4cb0ccd062
a-honey <jungahyeon0512@khu.ac.kr>
2024-02-02 02:38:10 +0900

    feat: 타입 추가 및 타입에 따른 스토리 추가

--- src/stores/toastAtom.ts
+++ src/stores/toastAtom.ts
@@ -1,9 +1,18 @@
 import { atom } from 'recoil';
 
-export const toastState = atom({
+export type ToastType = 'info' | 'success' | 'error';
+
+export interface ToastState {
+  message: string;
+  isOpen: boolean;
+  type: ToastType;
+}
+
+export const toastState = atom<ToastState>({
   key: 'toastState',
   default: {
     message: '',
     isOpen: false,
+    type: 'success',
   },
 });
