6f6bdf309f8b0978215e240ccc24a79f26f8e2dd
a-honey <jungahyeon0512@khu.ac.kr>
2024-02-02 12:44:32 +0900

    fix: 타입에 따른 배경색상 변경

--- src/components/atoms/ToastMessage.tsx
+++ src/components/atoms/ToastMessage.tsx
@@ -1,12 +1,26 @@
 import { toastState } from '@/stores/toastState';
+import type { ToastType } from '@/stores/toastState';
 import React, { useEffect } from 'react';
 import { useRecoilState } from 'recoil';
 
 const ToastMessage: React.FC = () => {
   const [toast, setToast] = useRecoilState(toastState);
 
+  const getBackgroundColor = (type: ToastType): string => {
+    switch (type) {
+      case 'info':
+        return 'bg-black bg-opacity-70';
+      case 'success':
+        return 'bg-black bg-opacity-70';
+      case 'error':
+        return 'bg-point4 bg-opacity-70';
+      default:
+        return 'bg-black bg-opacity-70';
+    }
+  };
+
   const closeToast: () => void = () => {
-    setToast((prev) => ({ ...prev, isOpen: false }));
+    setToast((prevToast) => ({ ...prevToast, isOpen: false }));
   };
 
   useEffect(() => {
@@ -20,7 +34,9 @@ const ToastMessage: React.FC = () => {
   }, []);
 
   return (
-    <div className="fixed bottom-[90px] left-[20px] right-[20px] p-[16px] rounded-[12px] text-center text-white body1-regular bg-black bg-opacity-70">
+    <div
+      className={`fixed bottom-[90px] left-[20px] right-[20px] p-[16px] rounded-[12px] text-center text-white body1-regular ${getBackgroundColor(toast.type)}`}
+    >
       {toast.message}
     </div>
   );
