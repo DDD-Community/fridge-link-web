ea109e9b881d39f32c1801d0bef4559170f91d3a
hyeseon han <hyeseon405@gmail.com>
2024-02-02 23:12:05 +0900

    chore: Header component barrel 파일에 추가

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -6,4 +6,8 @@ export { default as FridgeListModal } from './FridgeListModal';
 export { default as FriendsRecentBoard } from './FriendsRecentBoard';
 export { default as FriendsFridgeList } from './FriendsFridgeList';
 export { default as OrderListModal } from './OrderListModal';
+<<<<<<< HEAD
 export { default as NavWhiteBox } from './NavWhiteBox';
+=======
+export { default as Header } from './Header';
+>>>>>>> bf6e330 (chore: Header component barrel 파일에 추가)
