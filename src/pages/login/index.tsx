5a57d22a591f9dfdd761e73313a6833da2e19dd5
a-honey <jungahyeon0512@khu.ac.kr>
2024-02-03 04:10:52 +0900

    feat: 카카오 code 받아오기

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,8 +2,23 @@ import MonstersImg from '@/assets/images/img_login_monsters.svg';
 import KaKaoImg from '@/assets/images/img_login_kakao.svg';
 import GoogleImg from '@/assets/images/img_login_google.svg';
 import { type NextPage } from 'next';
+import { useEffect } from 'react';
+
+const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code`;
 
 const LoginPage: NextPage = () => {
+  const handleKaKaoClick: () => void = () => {
+    window.location.href = kakaoURL;
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get('code');
+
+    if (code) {
+      console.log('받아온 코드:', code);
+    }
+  }, []);
   return (
     <section
       className={`flex flex-col items-center justify-evenly min-h-screen p-20 bg-white`}
@@ -22,7 +37,7 @@ const LoginPage: NextPage = () => {
           <div className="flex-1 w-[96px] h-[1px] bg-gray6"></div>
         </div>
         <div className="flex gap-[20px]">
-          <KaKaoImg />
+          <KaKaoImg onClick={handleKaKaoClick} />
           <GoogleImg />
         </div>
       </div>
