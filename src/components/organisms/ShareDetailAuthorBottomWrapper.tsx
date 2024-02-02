65ed73b5659d3674c9ff0be736893bca6eb59d8d
hyeseon han <hyeseon405@gmail.com>
2024-02-03 00:34:44 +0900

    feat: 나눔 상세 화면 구현

---
+++ src/components/organisms/ShareDetailAuthorBottomWrapper.tsx
@@ -0,0 +1,139 @@
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalOverlay,
+  useDisclosure,
+} from '@chakra-ui/react';
+
+import { Button, RadioButtonField } from '../atoms';
+import React from 'react';
+import { type SortLabel } from '@/types/common';
+
+const SHARE_STATUSES = [
+  { label: '나눔 신청', value: 'enroll' },
+  { label: '나눔 중', value: 'proceeding' },
+  { label: '나눔 완료', value: 'complete' },
+];
+
+const MOCK_DATA_PARTICIPANTS = [
+  '김지수',
+  '김지수',
+  '김지수',
+  '김지수',
+  '김지수',
+  '김지수',
+  '김지수',
+  '김지수',
+  '김지수',
+  '김지수',
+  '김지수',
+  '김지수',
+  '김지수',
+  '김지수',
+  '김지수',
+];
+
+const ShareDetailAuthorBottomWrapper: React.FC<{
+  curStatus: SortLabel;
+  onChangeStatus: React.Dispatch<React.SetStateAction<SortLabel>>;
+}> = ({ curStatus, onChangeStatus }) => {
+  const {
+    isOpen: isStatusModalOpen,
+    onOpen: onStatusModalOpen,
+    onClose: onStatusModalClose,
+  } = useDisclosure();
+  const {
+    isOpen: isParticipantsModalOpen,
+    onOpen: onParticipantsModalOpen,
+    onClose: onParticipantsModalClose,
+  } = useDisclosure();
+
+  return (
+    <>
+      <div className="fixed flex gap-[11px] w-full max-w-[480px] bottom-0 p-[20px] pb-[32px] bg-gray1">
+        <button
+          onClick={onStatusModalOpen}
+          className="w-full text-center py-[16px] rounded-[12px] text-gray6 bg-gray2 heading4-semibold"
+        >
+          상태 변경
+        </button>
+        <button
+          onClick={onParticipantsModalOpen}
+          className="w-full text-center py-[16px] rounded-[12px] text-white bg-primary2 heading4-semibold"
+        >
+          신청인원 확인
+        </button>
+      </div>
+
+      <Modal
+        onClose={onStatusModalClose}
+        isOpen={isStatusModalOpen}
+        motionPreset="slideInBottom"
+        trapFocus={false}
+      >
+        <ModalOverlay height="100vh" onClick={onStatusModalClose} />
+        <ModalContent
+          className=" bg-white"
+          position="fixed"
+          bottom="0"
+          borderRadius="24px 24px 0px 0px"
+          maxW="lg"
+        >
+          <ModalBody>
+            {SHARE_STATUSES.map((ele) => (
+              <RadioButtonField
+                key={ele.value}
+                label={ele.label}
+                onClick={() => {
+                  onChangeStatus(ele);
+                }}
+                checked={ele.value === curStatus.value}
+              />
+            ))}
+            <div className="px-[20px] pb-[32px]">
+              <Button
+                className="block w-full bg-primary2"
+                text={'선택 완료'}
+                onClick={onStatusModalClose}
+              />
+            </div>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+
+      <Modal
+        onClose={onStatusModalClose}
+        isOpen={isParticipantsModalOpen}
+        motionPreset="slideInBottom"
+        trapFocus={false}
+      >
+        <ModalOverlay height="100vh" onClick={onParticipantsModalClose} />
+        <ModalContent
+          className=" bg-white"
+          position="fixed"
+          bottom="0"
+          borderRadius="24px 24px 0px 0px"
+          maxW="lg"
+        >
+          <ModalBody>
+            <div className="max-h-[300px] overflow-scroll px-[20px] py-[40px]">
+              {MOCK_DATA_PARTICIPANTS.map((ele) => (
+                <div>{ele}</div>
+              ))}
+            </div>
+            <div className="p-[20px] pb-[32px]">
+              <Button
+                className="block w-full bg-primary2"
+                text={'확인'}
+                onClick={onParticipantsModalClose}
+              />
+            </div>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default ShareDetailAuthorBottomWrapper;
--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -6,8 +6,6 @@ export { default as FridgeListModal } from './FridgeListModal';
 export { default as FriendsRecentBoard } from './FriendsRecentBoard';
 export { default as FriendsFridgeList } from './FriendsFridgeList';
 export { default as OrderListModal } from './OrderListModal';
-<<<<<<< HEAD
 export { default as NavWhiteBox } from './NavWhiteBox';
-=======
 export { default as Header } from './Header';
->>>>>>> bf6e330 (chore: Header component barrel 파일에 추가)
+export { default as ShareDetailAuthorBottomWrapper } from './ShareDetailAuthorBottomWrapper';
