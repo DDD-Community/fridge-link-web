6d52a1c4761af684b7393b82bef0a55e8482d292
a-honey <jungahyeon0512@khu.ac.kr>
2024-02-02 12:14:23 +0900

    chore: 테스트용 스토리 삭제

--- src/stories/Test.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Test from './Test';
-
-export default {
-  title: 'Components/Test',
-  component: Test,
-};
-
-export const Default: React.FC = (args: any) => <Test {...args} />;
