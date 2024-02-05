e31832ab50545d84188343bb864bd19f88968db4
a-honey <jungahyeon0512@khu.ac.kr>
2024-02-05 21:50:30 +0900

    feat: 데이터 패칭 에러 확인용
    Network Eror

--- src/hooks/queries/fridge/useGetIngredientList.ts
+++ src/hooks/queries/fridge/useGetIngredientList.ts
@@ -1,6 +1,7 @@
 import axiosInstance from '@/api/axiosInstance';
 import type { IngredientType } from '@/types/fridge';
 import { useQuery } from '@tanstack/react-query';
+import useToast from '../../useToast';
 
 interface IngredientListResponse {
   data: IngredientType[];
@@ -9,10 +10,16 @@ interface IngredientListResponse {
 export const fetchIngredientList = async (): Promise<IngredientListResponse> =>
   await axiosInstance.get(`/ingrs`);
 
-export const useGetIngredientList: () => void = () => {
+export const useGetIngredientList: () => any = () => {
+  const { showToast } = useToast();
   const { data, isLoading } = useQuery({
     queryKey: ['ingredientList'],
-    queryFn: async () => await fetchIngredientList().then((res) => res.data),
+    queryFn: async () =>
+      await fetchIngredientList()
+        .then((res) => res.data)
+        .catch(() => {
+          showToast('식자재 불러오기에 실패했습니다.', 'success');
+        }),
   });
 
   return { data, isLoading };
