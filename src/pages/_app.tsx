c4ee632d26bc3792d500894f3e7568831b49f156
a-honey <jungahyeon0512@khu.ac.kr>
2024-02-07 01:41:18 +0900

    feat: 전역 QueryClientProvider 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import Layout from '@/components/templates/Layout';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import 'dayjs/locale/ko';
 import dayjs from 'dayjs';
 dayjs.locale('ko');
@@ -13,15 +14,26 @@ const theme = extendTheme({
   },
 });
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <CSSReset />
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ThemeProvider>
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <ThemeProvider theme={theme}>
+          <CSSReset />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ThemeProvider>
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 }
