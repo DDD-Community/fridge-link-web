51eb41d0ee1da387e8600fea6b444dcbab20bc2e
a-honey <jungahyeon0512@khu.ac.kr>
2024-02-07 02:32:49 +0900

    feat: 카카오 로그인 api 요청
    Network Error

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,27 +3,36 @@ import KaKaoImg from '@/assets/images/img_login_kakao.svg';
 import GoogleImg from '@/assets/images/img_login_google.svg';
 import { type NextPage } from 'next';
 import { useEffect } from 'react';
+import { getKaKaoToken } from '@/api/login/getToken';
 
 const LoginPage: NextPage = () => {
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code`;
   const googleURL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI}&response_type=code&scope=email&access_type=offline`;
 
   const handleKaKaoClick: () => void = () => {
-    window.location.href = kakaoURL;
+    window.location.href = `${kakaoURL}&type=kakao`;
   };
 
   const handleGoogleClick: () => void = () => {
-    window.location.href = googleURL;
+    window.location.href = `${googleURL}&type=google`;
   };
 
   useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const code = urlParams.get('code');
+    const fetchData = async (): Promise<void> => {
+      const urlParams = new URLSearchParams(window.location.search);
+      const code = urlParams.get('code');
+      // const type = urlParams.get('type');
+      // 구글 추가시 타입 redirect_uri 변경
 
-    if (code) {
-      console.log('받아온 코드:', code);
-    }
+      if (code) {
+        await getKaKaoToken(code);
+      }
+    };
+
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    fetchData();
   }, []);
+
   return (
     <section
       className={`flex flex-col items-center justify-evenly min-h-screen p-20 bg-white`}
