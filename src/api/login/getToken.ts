18667e1625df42ec8168f66fc533160292fb43b0
a-honey <jungahyeon0512@khu.ac.kr>
2024-02-09 05:18:13 +0900

    fix: 에러 .catch 제거

--- src/api/login/getToken.ts
+++ src/api/login/getToken.ts
@@ -8,24 +8,16 @@ const fetchKaKao = async (code: string): Promise<LoginResponseType> =>
   await axiosInstance.get(`/users/kakao-login?code=${code}`);
 
 export const getKaKaoToken: (code: string) => Promise<void> = async (code) => {
-  fetchKaKao(code)
-    .then((response: LoginResponseType) => {
-      localStorage.setItem('token', response.data.accessToken);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  void fetchKaKao(code).then((response: LoginResponseType) => {
+    localStorage.setItem('token', response.data.accessToken);
+  });
 };
 
 const fetchGoogle = async (code: string): Promise<LoginResponseType> =>
   await axiosInstance.get(`/users/google-login?code=${code}`);
 
 export const getGoogleToken: (code: string) => Promise<void> = async (code) => {
-  fetchGoogle(code)
-    .then((response: LoginResponseType) => {
-      localStorage.setItem('token', response.data.accessToken);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  void fetchGoogle(code).then((response: LoginResponseType) => {
+    localStorage.setItem('token', response.data.accessToken);
+  });
 };
