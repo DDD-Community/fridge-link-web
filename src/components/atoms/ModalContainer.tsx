7be7d3df8194a59110514f74d4c7e01a21b36851
a-honey <jungahyeon0512@khu.ac.kr>
2024-02-17 01:28:13 +0900

    fix: 모달컨테이너 article 태그, div 스타일 변경

--- src/components/atoms/ModalContainer.tsx
+++ src/components/atoms/ModalContainer.tsx
@@ -6,12 +6,10 @@ interface ModalBoxProps {
 
 const ModalContainer: React.FC<ModalBoxProps> = ({ children }) => {
   return (
-    <div className="p-[20px] pt-[40px]">
-      <div className="relative">
-        <div className="absolute top-[-16px] right-[200px] left-[200px] h-[4px] bg-gray-500 rounded-[100px]"></div>
-      </div>
+    <article className="p-[20px] pt-[40px] relative">
+      <div className="absolute top-[16px] right-[200px] left-[200px] h-[4px] bg-gray-500 rounded-[100px]"></div>
       {children}
-    </div>
+    </article>
   );
 };
 
