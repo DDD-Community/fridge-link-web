985f4e976fd42b4f52b86d57213f4d1c850daa87
a-honey <jungahyeon0512@khu.ac.kr>
2024-02-22 05:17:42 +0900

    feat: 내 냉장고 추가 api 연결

--- src/components/organisms/FridgeListModal.tsx
+++ src/components/organisms/FridgeListModal.tsx
@@ -5,6 +5,7 @@ import React, { useState } from 'react';
 import { FridgeListItem } from '../molecules';
 import useGetMyFridgeList from '@/hooks/queries/fridge/useGetFridgeList';
 import { useRouter } from 'next/router';
+import usePostFridge from '@/hooks/queries/fridge/usePostFridge';
 
 const FridgeListModal: React.FC<{
   isMyFridgeList?: boolean;
@@ -16,9 +17,16 @@ const FridgeListModal: React.FC<{
 
   const router = useRouter();
   const fridgeList = useGetMyFridgeList();
+  const fridgeMutation = usePostFridge();
 
   const handleFridgeClick = (id: number) => {
     void router.push(`?fridge-id=${id}`);
+  };
+
+  const handleNewFridgeClick = () => {
+    fridgeMutation.mutate({
+      name: `내 냉장고 ${fridgeList !== null && fridgeList !== undefined ? fridgeList[fridgeList.length - 1].id + 1 : 1}`,
+    });
   };
 
   return (
@@ -40,8 +48,10 @@ const FridgeListModal: React.FC<{
             }}
           />
         ))}
-
-        <button className="flex justify-center items-center h-[64px] border-2 rounded-[12px] text-gray3">
+        <button
+          onClick={handleNewFridgeClick}
+          className="flex justify-center items-center h-[64px] border-2 rounded-[12px] text-gray3"
+        >
           <PlusIcon />
           냉장고 추가
         </button>
