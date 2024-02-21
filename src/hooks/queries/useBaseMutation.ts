7c4748901168518f8202e0f818d70a683b56ed71
a-honey <jungahyeon0512@khu.ac.kr>
2024-02-22 06:29:37 +0900

    fix: 뮤테이션 메서드 추가

--- src/hooks/queries/useBaseMutation.ts
+++ src/hooks/queries/useBaseMutation.ts
@@ -1,8 +1,13 @@
 import axiosInstance from '@/api/axiosInstance';
 import { useMutation } from '@tanstack/react-query';
 
-export const fetchData = async <T>(url: string, body: T) => {
-  const response = await axiosInstance.post<{ data: T }>(url, body);
+export const fetchData = async <T>(url: string, body: T, method: string) => {
+  const response = await axiosInstance.request({
+    method,
+    url,
+    data: body,
+  });
+
   return response.data;
 };
 
@@ -10,11 +15,12 @@ export const useBaseMutation = <T>(
   mutationKey: any,
   url: string,
   onSuccess: (any: any) => void,
+  method: 'POST' | 'PUT' | 'DELETE' = 'POST',
 ) => {
   return useMutation({
     mutationKey,
     mutationFn: async (body: T) => {
-      const response = await fetchData<T>(url, body);
+      const response = await fetchData<T>(url, body, method);
 
       onSuccess(response.data);
     },
