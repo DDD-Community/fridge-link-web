2a4c9adb484bf841917da925c905f53cc1894b94
hyeseon han <hyeseon405@gmail.com>
2024-02-23 23:33:33 +0900

    feat: 친구 목록 조회 API profileImage Enum 필드값 추가 대응

--- src/types/friendship/index.d.ts
+++ src/types/friendship/index.d.ts
@@ -1,7 +1,10 @@
+import type { ProfileEnum } from '../common';
+
 export interface FriendshipData {
   userId: number;
   nickname: string;
   ingredientCount: number;
+  profileImage: ProfileEnum;
 }
 
 export type FriendshipSortType = 'nickname' | 'createdAt';
