129a5ffeed5696666874e47a8d945893d0d57ee9
a-honey <jungahyeon0512@khu.ac.kr>
2024-02-25 02:16:03 +0900

    fix: 기타 작업

--- src/components/organisms/FridgeInfoBox.tsx
+++ src/components/organisms/FridgeInfoBox.tsx
@@ -13,12 +13,14 @@ const FridgeInfoBox: React.FC<{
   return (
     <div className="flex justify-between items-end mb-[28px]">
       <div className="flex flex-col gap-[12px]">
-        <div className="body1-medium text-gray7">{userName}님의</div>
+        <div className="body1-medium text-gray7">
+          {userName ?? '사용자정보없음'} 님의
+        </div>
         <div
           className="flex items-center gap-[8px]"
           onClick={toggleIsOpenFridgeListModal}
         >
-          <div className="heading1-bold">{name}</div>
+          <div className="heading1-bold">{name ?? '냉장고를 선택해주세요'}</div>
           <AngleIcon
             width={16}
             height={16}
