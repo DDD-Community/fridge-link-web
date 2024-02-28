610121825cdc8248a6454db8093d6896e8eeff42
hyeseon han <hyeseon405@gmail.com>
2024-02-29 00:52:24 +0900

    feat: usePostUpload hook 추가

--- src/hooks/queries/queryKeys.ts
+++ src/hooks/queries/queryKeys.ts
@@ -1,18 +1,10 @@
-import type {
-  FriendshipSortType,
-  ShareSortType,
-  ShareStatusType,
-} from '@/types/friendship';
+import type { FriendshipSortType, ShareSortType, ShareStatusType } from '@/types/friendship';
 
 export const queryKeys = {
   COUNT: () => ['count'],
   MY_FRIDGE_LIST: () => ['my_fridge_list'],
   MY_FRIENDS_COUNT: () => ['my_friends_count'],
-  MY_FRIDGE_CONTENT: (id: number, location: string) => [
-    'my_fridge',
-    id,
-    location,
-  ],
+  MY_FRIDGE_CONTENT: (id: number, location: string) => ['my_fridge', id, location],
   MY_INGREDIENTS_COUNT: () => ['my_ingredient_count'],
   MY_INGREDIENT_ID: (id: number) => ['my-ingredient', id],
   MY_FRIENDS_NEWS: () => ['my_friends_news'],
@@ -22,16 +14,8 @@ export const queryKeys = {
   INGREDIENTS_RECENT: () => ['my-ingredient', 'recent'],
   KAKAO: () => ['kakao'],
   GOOGLE: () => ['google'],
-  SHARES: (sort: ShareSortType, status: ShareStatusType) => [
-    'shares',
-    sort,
-    status,
-  ],
-  MY_SHARES: (sort: ShareSortType, status: ShareStatusType) => [
-    'my-shares',
-    sort,
-    status,
-  ],
+  SHARES: (sort: ShareSortType, status: ShareStatusType) => ['shares', sort, status],
+  MY_SHARES: (sort: ShareSortType, status: ShareStatusType) => ['my-shares', sort, status],
   ME: () => ['my-info'],
   FRIENDSHIPS: (sort: FriendshipSortType) => ['friendship', sort],
   DELETE_FRIENDSHIP: () => ['deleteFriendship'],
@@ -39,6 +23,8 @@ export const queryKeys = {
   ADD_FRIENDSHIP: () => ['addFriendship'],
   SHARE_DETAIL: () => ['shareDetail'],
   SHARE_APPLICANTS: () => ['shareApplicants'],
+  UPLOAD: () => ['upload'],
+  ADD_SHARE: () => ['addShare'],
 } as const;
 
 export type QueryKeys = (typeof queryKeys)[keyof typeof queryKeys];
--- src/hooks/queries/share/index.ts
+++ src/hooks/queries/share/index.ts
@@ -1,3 +1,4 @@
 export { default as useGetShares } from './useGetShares';
 export { default as useGetShareDetail } from './useGetShareDetail';
 export { default as useGetShareApplicants } from './useGetShareApplicants';
+export { default as usePostUpload } from './usePostUpload';
