eaa6692b0fa6bb2847e82f8b064c56a24a19d181
Ahyeon, Jung <75254185+a-honey@users.noreply.github.com>
2024-03-01 14:37:42 +0900

    hotfix: 도메인 추가 (#37)
    * fix: 카카오로그인 도메인 status 추가
    
    * fix: 워크플로우 BASE_URL 추가

--- src/hooks/queries/login/useGetKakaoToken.ts
+++ src/hooks/queries/login/useGetKakaoToken.ts
@@ -4,12 +4,13 @@ import { useRouter } from 'next/router';
 
 const useGetKakaoToken = (code: string | null = '') => {
   const router = useRouter();
+  const status = window.location.href.split('//')[1].split('/')[0] === 'localhost:3000' ? 'dev' : 'prod';
   const { data } = useBaseQuery<{
     accessToken: string;
     refreshToken: string;
     kakaoId: number;
     kakaoEmail: string;
-  }>(queryKeys.KAKAO(), `/users/kakao-login?code=${code}`, true);
+  }>(queryKeys.KAKAO(), `/users/kakao-login?code=${code}&status=${status}`, true);
 
   if (data?.data?.accessToken === undefined) {
     void router.push(`/mypage/profile?kakaoId=${data?.data?.kakaoId}&kakaoEmail=${data?.data?.kakaoEmail}`);
